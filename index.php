f96341cd8f6e8e660c9062531648957e45ac6c45
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-04-05 14:18:16 +0000

    added php changes 2

--- index.php
+++ index.php
@@ -1,32 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Area of a Trapezoid, in JS" />
+  <meta name="description" content="Area of a Trapezoid, in PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Kyle Matthew" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="./css/style.css " />
+  <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
   <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Area of a Trapezoid, in JS</title>
+  <title>Area of a Trapezoid, in PHP</title>
 </head>
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Area of a Trapezoid, in JS</span>
-      </div>
-      <div>
-        <link rel="stylesheet" type="text/css" href="styles.css" />
+        <span class="mdl-layout-title">Area of a Trapezoid, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
@@ -35,24 +31,24 @@
       </div>
       <div class="page-content">Formula:</div>
       <br />
-      <form action="answer.php" method="POST">
+      <form action="answer.php" method="GE"> <!-- Set the form action to answer.php -->
         <p>A = [(a + b) / 2] * h</p>
         <div class="page-content">
           Please enter integers for dimensions:
         </div>
         <div class="mdl-textfield mdl-js-textfield">
           <p>a base:</p>
-          <input class="mdl-textbox__input" type="text" name="a_base" pattern="-?[0-9]*(\.[0-9]+)?" id="a-base-of-a-trapezoid" required />
+          <input class="mdl-textbox__input" type="text" name="a_base" pattern="-?[0-9]*(\.[0-9]+)?" name="base-a-of-trapizoid" required />
           <span> mm </span>
         </div>
         <p>b base:</p>
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textbox__input" type="text" name="b_base" pattern="-?[0-9]*(\.[0-9]+)?" id="b-base-of-a-trapezoid" required />
+          <input class="mdl-textbox__input" type="text" name="b_base" pattern="-?[0-9]*(\.[0-9]+)?" name="base-b-of-trapizoid" required />
           <span> mm </span>
         </div>
-        <p>height</p>
+        <p>height:</p>
         <div class="mdl-textfield mdl-js-textfield">
-          <input class="mdl-textbox__input" type="text" name="height" pattern="-?[0-9]*(\.[0-9]+)?" id="height-of-a-trapezoid" required />
+          <input class="mdl-textbox__input" type="text" name="height" pattern="-?[0-9]*(\.[0-9]+)?" name="height-of-a-trapezoid" required />
           <span> mm </span>
         </div>
         <br />
